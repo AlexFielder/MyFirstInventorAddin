531c094377a70da99db81acb6351755cf78009e9
the-war-horse <jameslrsmith@gmail.com>
2017-07-18 08:40:45 +0100

    Sort of fixed the issue by adding public boolean... But it's not super duper yet!

--- MyFirstInventorAddin/iPropertiesForm.Designer.vb
+++ MyFirstInventorAddin/iPropertiesForm.Designer.vb
@@ -338,7 +338,7 @@ Partial Class iPropertiesForm
         Me.Label13.Name = "Label13"
         Me.Label13.Size = New System.Drawing.Size(97, 10)
         Me.Label13.TabIndex = 315
-        Me.Label13.Text = "iProperties Controller v9.14"
+        Me.Label13.Text = "iProperties Controller v9.15"
         '
         'tbDrawnBy
         '
--- MyFirstInventorAddin/iPropertiesForm.vb
+++ MyFirstInventorAddin/iPropertiesForm.vb
@@ -19,6 +19,7 @@ Public Class iPropertiesForm
     Private inventorApp As Inventor.Application
     Private localWindow As DockableWindow
     Private value As String
+    Public ButtonPushed As Boolean = False
 
     Public ReadOnly log As ILog = LogManager.GetLogger(GetType(iPropertiesForm))
 
@@ -1441,13 +1442,15 @@ Public Class iPropertiesForm
 
     Private Sub tbDescription_Leave(sender As Object, e As EventArgs) Handles tbDescription.Leave
         If Not inventorApp.ActiveDocument Is Nothing Then
-            'If tbDescription.Text = "" Then
-            '    tbDescription.Text = "Description"
-            '    iProperties.GetorSetStandardiProperty(inventorApp.ActiveDocument, PropertiesForDesignTrackingPropertiesEnum.kDescriptionDesignTrackingProperties, "", "")
-            'End If
-            tbDescription.ForeColor = Drawing.Color.Black
-            tbDescription.SelectionStart = 0
-            CheckForDefaultAndUpdate(PropertiesForDesignTrackingPropertiesEnum.kDescriptionDesignTrackingProperties, "Description", tbDescription.Text)
+            If ButtonPushed = True Then
+                tbDescription.ForeColor = Drawing.Color.Black
+                CheckForDefaultAndUpdate(PropertiesForDesignTrackingPropertiesEnum.kDescriptionDesignTrackingProperties, "Description", tbDescription.Text)
+            Else
+                tbDescription.ForeColor = Drawing.Color.Black
+                tbDescription.SelectionStart = 0
+                CheckForDefaultAndUpdate(PropertiesForDesignTrackingPropertiesEnum.kDescriptionDesignTrackingProperties, "Description", tbDescription.Text)
+            End If
+            ButtonPushed = False
         End If
     End Sub
 
@@ -1645,19 +1648,12 @@ Public Class iPropertiesForm
         If e.KeyChar = Chr(9) Then
             btUpdateAll.Focus()
 
-            'ElseIf e.KeyChar = Chr(33 And 38) Then
-            '    tbStockNumber.Focus()
         ElseIf e.KeyChar = Chr(13) Then
             tbDrawnBy_Leave(sender, e)
         End If
     End Sub
 
     Private Sub tbRevNo_Leave(sender As Object, e As EventArgs) Handles tbRevNo.Leave
-        'If Not inventorApp.ActiveDocument Is Nothing Then
-        '    tbRevNo.ForeColor = Drawing.Color.Black
-        '    CheckForDefaultAndUpdate(PropertiesForSummaryInformationEnum.kRevisionSummaryInformation, "Revision Number", tbRevNo.Text)
-        'End If
-
         If Not inventorApp.ActiveDocument Is Nothing Then
             If TypeOf (inventorApp.ActiveDocument) Is DrawingDocument Then
                 Dim oDWG As DrawingDocument = AddinGlobal.InventorApp.ActiveDocument
@@ -1828,190 +1824,13 @@ Public Class iPropertiesForm
     End Sub
 
     Private Sub btDiaDes_Click(sender As Object, e As EventArgs) Handles btDiaDes.Click
-        'Dim insertText = "Ø"
-        'Dim insertPos As Integer = tbDescription.SelectionStart
-        'Dim focusPoint = insertPos + insertText.Length
-        'If tbDescription.Text = "Description" Then
-        '    tbDescription.Text = insertText
-        '    tbDescription.Focus()
-        '    tbDescription.Select(insertPos + insertText.Length, 0)
-        'Else
-        '    tbDescription.Text = tbDescription.Text.Insert(insertPos, insertText)
-        '    tbDescription.Focus()
-        '    tbDescription.Select(insertPos + insertText.Length, 0)
-        'End If
+        ButtonPushed = True
         SendSymbol(tbDescription, "Ø")
     End Sub
 
     Private Sub btDegDes_Click(sender As Object, e As EventArgs) Handles btDegDes.Click
-        'Dim insertText = "°"
-        'Dim insertPos As Integer = tbDescription.SelectionStart
-        'If tbDescription.Text = "Description" Then
-        '    tbDescription.Text = insertText
-        '    tbDescription.Focus()
-        '    tbDescription.Select(insertPos + insertText.Length, 0)
-        'Else
-        '    tbDescription.Text = tbDescription.Text.Insert(insertPos, insertText)
-        '    tbDescription.Focus()
-        '    tbDescription.Select(insertPos + insertText.Length, 0)
-        'End If
+        ButtonPushed = True
         SendSymbol(tbDescription, "°")
     End Sub
 
-    'Private Sub btUpdateAssy_Click(sender As Object, e As EventArgs)
-    '    If RefDoc.DocumentType = DocumentTypeEnum.kAssemblyDocumentObject Then
-    '        inventorApp.CommandManager.ControlDefinitions.Item("AssemblyGlobalUpdateCmd").Execute()
-    '        inventorApp.CommandManager.ControlDefinitions.Item("AppGlobalUpdateWrapperCmd").Execute()
-    '    Else
-    '        UpdateStatusBar("Can't update a part!! Come on, you know this!")
-    '    End If
-    'End Sub
 End Class
-
-'-------------------------------------------------------------------------------
-' Resizer
-' This class is used to dynamically resize and reposition all controls on a form.
-' Container controls are processed recursively so that all controls on the form
-' are handled.
-'
-' Usage:
-'  Resizing functionality requires only three lines of code on a form:
-'
-'  1. Create a form-level reference to the Resize class:
-'     Dim myResizer as Resizer
-'
-'  2. In the Form_Load event, call the  Resizer class FIndAllControls method:
-'     myResizer.FindAllControls(Me)
-'
-'  3. In the Form_Resize event, call the  Resizer class ResizeAllControls method:
-'     myResizer.ResizeAllControls(Me)
-'
-'-------------------------------------------------------------------------------
-'Public Class Resizer
-
-'    '----------------------------------------------------------
-'    ' ControlInfo
-'    ' Structure of original state of all processed controls
-'    '----------------------------------------------------------
-'    Private Structure ControlInfo
-'        Public name As String
-'        Public parentName As String
-'        Public leftOffsetPercent As Double
-'        Public topOffsetPercent As Double
-'        Public heightPercent As Double
-'        Public originalHeight As Integer
-'        Public originalWidth As Integer
-'        Public widthPercent As Double
-'        Public originalFontSize As Single
-'    End Structure
-
-'    '-------------------------------------------------------------------------
-'    ' ctrlDict
-'    ' Dictionary of (control name, control info) for all processed controls
-'    '-------------------------------------------------------------------------
-'    Private ctrlDict As Dictionary(Of String, ControlInfo) = New Dictionary(Of String, ControlInfo)
-
-'    '----------------------------------------------------------------------------------------
-'    ' FindAllControls
-'    ' Recursive function to process all controls contained in the initially passed
-'    ' control container and store it in the Control dictionary
-'    '----------------------------------------------------------------------------------------
-'    Public Sub FindAllControls(thisCtrl As Control)
-
-'        '-- If the current control has a parent, store all original relative position
-'        '-- and size information in the dictionary.
-'        '-- Recursively call FindAllControls for each control contained in the
-'        '-- current Control
-'        For Each ctl As Control In thisCtrl.Controls
-'            Try
-'                If Not IsNothing(ctl.Parent) Then
-'                    Dim parentHeight = ctl.Parent.Height
-'                    Dim parentWidth = ctl.Parent.Width
-
-'                    Dim c As New ControlInfo
-'                    c.name = ctl.Name
-'                    c.parentName = ctl.Parent.Name
-'                    c.topOffsetPercent = Convert.ToDouble(ctl.Top) / Convert.ToDouble(parentHeight)
-'                    c.leftOffsetPercent = Convert.ToDouble(ctl.Left) / Convert.ToDouble(parentWidth)
-'                    c.heightPercent = Convert.ToDouble(ctl.Height) / Convert.ToDouble(parentHeight)
-'                    c.widthPercent = Convert.ToDouble(ctl.Width) / Convert.ToDouble(parentWidth)
-'                    c.originalFontSize = ctl.Font.Size
-'                    c.originalHeight = ctl.Height
-'                    c.originalWidth = ctl.Width
-'                    ctrlDict.Add(c.name, c)
-'                End If
-
-'            Catch ex As Exception
-'                Debug.Print(ex.Message)
-'            End Try
-
-'            If ctl.Controls.Count > 0 Then
-'                FindAllControls(ctl)
-'            End If
-
-'        Next '-- For Each
-
-'    End Sub
-
-'    '----------------------------------------------------------------------------------------
-'    ' ResizeAllControls
-'    ' Recursive function to resize and reposition all controls contained in the Control
-'    ' dictionary
-'    '----------------------------------------------------------------------------------------
-'    Public Sub ResizeAllControls(thisCtrl As Control)
-
-'        Dim fontRatioW As Single
-'        Dim fontRatioH As Single
-'        Dim fontRatio As Single
-'        Dim f As Font
-
-'        '-- Resize and reposition all controls in the passed control
-'        For Each ctl As Control In thisCtrl.Controls
-'            Try
-'                If Not IsNothing(ctl.Parent) Then
-'                    Dim parentHeight = ctl.Parent.Height
-'                    Dim parentWidth = ctl.Parent.Width
-
-'                    Dim c As New ControlInfo
-
-'                    Dim ret As Boolean = False
-'                    Try
-'                        '-- Get the current control's info from the control info dictionary
-'                        ret = ctrlDict.TryGetValue(ctl.Name, c)
-
-'                        '-- If found, adjust the current control based on control relative
-'                        '-- size and position information stored in the dictionary
-'                        If (ret) Then
-'                            '-- Size
-'                            ctl.Width = Int(parentWidth * c.widthPercent)
-'                            ctl.Height = Int(parentHeight * c.heightPercent)
-
-'                            '-- Position
-'                            ctl.Top = Int(parentHeight * c.topOffsetPercent)
-'                            ctl.Left = Int(parentWidth * c.leftOffsetPercent)
-
-'                            '-- Font
-'                            f = ctl.Font
-'                            fontRatioW = ctl.Width / c.originalWidth
-'                            fontRatioH = ctl.Height / c.originalHeight
-'                            fontRatio = (fontRatioW +
-'                            fontRatioH) / 2 '-- average change in control Height and Width
-'                            ctl.Font = New Font(f.FontFamily,
-'                            c.originalFontSize * fontRatio, f.Style)
-
-'                        End If
-'                    Catch
-'                    End Try
-'                End If
-'            Catch ex As Exception
-'            End Try
-
-'            '-- Recursive call for controls contained in the current control
-'            If ctl.Controls.Count > 0 Then
-'                ResizeAllControls(ctl)
-'            End If
-
-'        Next '-- For Each
-'    End Sub
-
-'End Class
