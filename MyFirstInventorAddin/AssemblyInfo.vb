5680e2459b1d3765b022cb70635a4c717833faca
James Smith <jameslrsmith@gmail.com>
2021-07-26 09:32:41 +0100

    rev up and build release.

--- MyFirstInventorAddin/AssemblyInfo.vb
+++ MyFirstInventorAddin/AssemblyInfo.vb
@@ -27,4 +27,4 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("15.0.2.*")>
+<Assembly: AssemblyVersion("15.0.3.*")>
